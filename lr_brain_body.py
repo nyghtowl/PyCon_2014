00630a7a9d210f39960b9a0eaed3c6692377c990
nyghtowl <warrick.melanie@gmail.com>
2014-04-08 20:39:15 -0700

    Added additional graphs and made changes on colors to improve how it looks on the slides.

--- lr_brain_body.py
+++ lr_brain_body.py
@@ -34,8 +34,8 @@ def predict_eval(model, X_test, y_test):
     print('Root Mean Squared Error: %.2f' % ((np.mean((model.predict(X_test) - y_test) ** 2))**(0.5)))
 
 
-def scatter_plot(model, X, y, title, fn):
-    plt.scatter(X, y,  color='black')
+def scatter_plot(model, X, y, title, fn, color):
+    plt.scatter(X, y,  color=color)
     plt.title(title, fontsize=16)
     plt.xlabel('Head Size (in^3)', fontsize=13)
     plt.ylabel('Brain Weight (lb)', fontsize=13)
@@ -45,8 +45,21 @@ def scatter_plot(model, X, y, title, fn):
     plt.savefig(fn)
     plt.show()
 
-def model_plot(model, X, y, title, fn1, fn2):
-    plt.scatter(X, y,  color='black')
+def scatter_split_plot(model, dataset, title, fn):
+    X_train, X_test, y_train, y_test = dataset
+    train = plt.scatter(X_train, y_train,  color='#fc4e2a')
+    test = plt.scatter(X_test, y_test,  color='#0c2c84')
+    plt.title(title, fontsize=16)
+    plt.xlabel('Head Size (in^3)', fontsize=13)
+    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    plt.xlim(180,280)
+    plt.ylim(2.4,3.8)
+    plt.legend([train,test],['Train Data', 'Test Data'], fontsize=13, loc='lower right')
+    plt.savefig(fn)
+    plt.show()
+
+def model_plot(model, X, y, title, fn, color):
+    plt.scatter(X, y,  color=color)
 
     plt.plot(X, model.predict(X), linewidth=3)
 
@@ -56,34 +69,44 @@ def model_plot(model, X, y, title, fn1, fn2):
     plt.xlim(180,280)
     plt.ylim(2.4,3.8)
 
-    plt.savefig(fn1)
+    plt.savefig(fn)
 
-    plt.plot([240,240],[2.4,3], color='r')
-    plt.plot([180,240],[3,3], color='r')
+    plt.show()
 
-    plt.savefig(fn2)
+def model_ex_predict(model, X, y, title, fn):
+    plt.plot(X, model.predict(X), linewidth=3)
 
+    plt.title(title, fontsize=16)
+    plt.xlabel('Head Size (in^3)', fontsize=13)
+    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    plt.xlim(180,280)
+    plt.ylim(2.4,3.8)
+
+    plt.plot([240,240],[2.4,3.03], color='r')
+    plt.plot([180,240],[3.03,3.03], color='r')
+
+    plt.savefig(fn)
 
     plt.show()
 
 def main():
     data = load_data('./data/both_brain_body.txt')    
     X, y = np.array(data['Head_Size']), np.array(data['Brain_Weight'])
-    X_train, X_test, y_train, y_test = create_datasets(X[:,np.newaxis], y, 6)
+    # 6 overfits
+    X_train, X_test, y_train, y_test = create_datasets(X[:,np.newaxis], y, 1)
 
     model = build_model(X_train, y_train)
     predict_eval(model, X_test, y_test)
 
-    scatter_plot(model, X, y, 'Linear Regression - Predict Brain Weight by Head Size', 'full_scatter_ex.png')
+    scatter_plot(model, X, y, 'Linear Regression - Predict Brain Weight by Head Size', 'full_scatter_ex.png', 'black')
     
-    scatter_plot(model, X_train, y_train, 'Training Data - Brain Weight & Head Size', 'train_scatter_ex.png')
+    scatter_split_plot(model, [X_train, X_test, y_train, y_test], 'Train & Test Data Split', 'split_scatter_ex.png')
 
-    model_plot(model, X_train, y_train,'Model with Training Data', 'train_model_ex.png', 'train_model_red_ex.png')
+    model_plot(model, X_train, y_train,'Fit Model with Training Data', 'train_model_ex.png', '#fc4e2a')
 
-    scatter_plot(model, X_test, y_test, 'Test Data - Brain Weight & Head Size', 'test_scatter_ex.png')
+    model_plot(model, X_test, y_test, 'Evaluate Model with Test Data', 'body_brain_lr_ex.png', '#0c2c84')
 
-
-    model_plot(model, X_test, y_test, 'Model Evaluation - Brain Weight & Head Size Training Data', 'body_brain_lr_ex.png','body_brain_lr_red_ex.png')
+    model_ex_predict(model, X_test, y_test, 'Example Brain Weight Prediction', 'body_brain_predict_ex.png')
 
     return model
 
