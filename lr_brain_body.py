c72f67a10b1a9774cdfd184be9b5fc22078937b5
nyghtowl <warrick.melanie@gmail.com>
2014-04-10 10:51:52 -0700

    Updated graph images. Updated script to create variations on model plot and scatter plot. There are ways to condense this. Will need to work on that at some point.

--- lr_brain_body.py
+++ lr_brain_body.py
@@ -35,40 +35,41 @@ def predict_eval(model, X_test, y_test):
 
 
 def scatter_plot(model, X, y, title, fn, color):
-    plt.scatter(X, y,  color=color)
-    plt.title(title, fontsize=16)
-    plt.xlabel('Head Size (in^3)', fontsize=13)
-    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    plt.scatter(X, y,  color=color, s=30)
+    plt.title(title, fontsize=24)
+    plt.xlabel('Head Size (in^3)', fontsize=18)
+    plt.ylabel('Brain Weight (lb)', fontsize=18)
     plt.xlim(180,280)
     plt.ylim(2.4,3.8)
-
+    plt.tick_params(labelsize=14)
     plt.savefig(fn)
     plt.show()
 
 def scatter_split_plot(model, dataset, title, fn):
     X_train, X_test, y_train, y_test = dataset
-    train = plt.scatter(X_train, y_train,  color='#fc4e2a')
-    test = plt.scatter(X_test, y_test,  color='#0c2c84')
-    plt.title(title, fontsize=16)
-    plt.xlabel('Head Size (in^3)', fontsize=13)
-    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    train = plt.scatter(X_train, y_train,  color='#fc4e2a', s=30)
+    test = plt.scatter(X_test, y_test,  color='#0c2c84', s=30)
+    plt.title(title, fontsize=24)
+    plt.xlabel('Head Size (in^3)', fontsize=18)
+    plt.ylabel('Brain Weight (lb)', fontsize=18)
     plt.xlim(180,280)
     plt.ylim(2.4,3.8)
-    plt.legend([train,test],['Train Data', 'Test Data'], fontsize=13, loc='lower right')
+    plt.tick_params(labelsize=14)
+    plt.legend([train,test],['Train Data', 'Test Data'], fontsize=16, loc='lower right')
     plt.savefig(fn)
     plt.show()
 
 def model_plot(model, X, y, title, fn, color):
-    plt.scatter(X, y,  color=color)
+    plt.scatter(X, y,  color=color, s=30)
 
     plt.plot(X, model.predict(X), linewidth=3)
 
-    plt.title(title, fontsize=16)
-    plt.xlabel('Head Size (in^3)', fontsize=13)
-    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    plt.title(title, fontsize=24)
+    plt.xlabel('Head Size (in^3)', fontsize=18)
+    plt.ylabel('Brain Weight (lb)', fontsize=18)
     plt.xlim(180,280)
     plt.ylim(2.4,3.8)
-
+    plt.tick_params(labelsize=14)
     plt.savefig(fn)
 
     plt.show()
@@ -76,15 +77,15 @@ def model_plot(model, X, y, title, fn, color):
 def model_ex_predict(model, X, y, title, fn):
     plt.plot(X, model.predict(X), linewidth=3)
 
-    plt.title(title, fontsize=16)
-    plt.xlabel('Head Size (in^3)', fontsize=13)
-    plt.ylabel('Brain Weight (lb)', fontsize=13)
+    plt.title(title, fontsize=24)
+    plt.xlabel('Head Size (in^3)', fontsize=18)
+    plt.ylabel('Brain Weight (lb)', fontsize=18)
     plt.xlim(180,280)
     plt.ylim(2.4,3.8)
 
     plt.plot([240,240],[2.4,3.03], color='r')
     plt.plot([180,240],[3.03,3.03], color='r')
-
+    plt.tick_params(labelsize=14)
     plt.savefig(fn)
 
     plt.show()
@@ -98,7 +99,7 @@ def main():
     model = build_model(X_train, y_train)
     predict_eval(model, X_test, y_test)
 
-    scatter_plot(model, X, y, 'Linear Regression - Predict Brain Weight by Head Size', 'full_scatter_ex.png', 'black')
+    scatter_plot(model, X, y, 'Brain Weight by Head Size Data', 'full_scatter_ex.png', 'black')
     
     scatter_split_plot(model, [X_train, X_test, y_train, y_test], 'Train & Test Data Split', 'split_scatter_ex.png')
 
@@ -106,7 +107,7 @@ def main():
 
     model_plot(model, X_test, y_test, 'Evaluate Model with Test Data', 'body_brain_lr_ex.png', '#0c2c84')
 
-    model_ex_predict(model, X_test, y_test, 'Example Brain Weight Prediction', 'body_brain_predict_ex.png')
+    model_ex_predict(model, X_train, y_train, 'Example Brain Weight Prediction', 'body_brain_predict_ex.png')
 
     return model
 
