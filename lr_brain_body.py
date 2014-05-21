b0001448fd7be0160f77d1d9d14493c84bcbffd6
nyghtowl <warrick.melanie@gmail.com>
2014-05-21 10:50:23 -0700

    Consolidated repetitive code on graph description setup to one function. Also reran the graphs to confirm it works.

--- lr_brain_body.py
+++ lr_brain_body.py
@@ -34,9 +34,7 @@ def predict_eval(model, X_test, y_test):
     print('Root Mean Squared Error: %.2f' % ((np.mean((model.predict(X_test) - y_test) ** 2))**(0.5)))
 
 
-def scatter_plot(model, X, y, title, fn, color):
-    plt.scatter(X, y,  color=color, s=30)
-    plt.title(title, fontsize=24)
+def design_plot(fn):
     plt.xlabel('Head Size (in^3)', fontsize=18)
     plt.ylabel('Brain Weight (lb)', fontsize=18)
     plt.xlim(180,280)
@@ -45,50 +43,35 @@ def scatter_plot(model, X, y, title, fn, color):
     plt.savefig(fn)
     plt.show()
 
+def scatter_plot(model, X, y, title, fn, color):
+    plt.scatter(X, y,  color=color, s=30)
+    plt.title(title, fontsize=24)
+    design_plot(fn)
+    
 def scatter_split_plot(model, dataset, title, fn):
     X_train, X_test, y_train, y_test = dataset
     train = plt.scatter(X_train, y_train,  color='#fc4e2a', s=30)
     test = plt.scatter(X_test, y_test,  color='#0c2c84', s=30)
     plt.title(title, fontsize=24)
-    plt.xlabel('Head Size (in^3)', fontsize=18)
-    plt.ylabel('Brain Weight (lb)', fontsize=18)
-    plt.xlim(180,280)
-    plt.ylim(2.4,3.8)
-    plt.tick_params(labelsize=14)
     plt.legend([train,test],['Train Data', 'Test Data'], fontsize=16, loc='lower right')
-    plt.savefig(fn)
-    plt.show()
+    design_plot(fn)
 
 def model_plot(model, X, y, title, fn, color):
     plt.scatter(X, y,  color=color, s=30)
 
     plt.plot(X, model.predict(X), linewidth=3)
 
-    plt.title(title, fontsize=24)
-    plt.xlabel('Head Size (in^3)', fontsize=18)
-    plt.ylabel('Brain Weight (lb)', fontsize=18)
-    plt.xlim(180,280)
-    plt.ylim(2.4,3.8)
-    plt.tick_params(labelsize=14)
-    plt.savefig(fn)
-
-    plt.show()
+    design_plot(fn)
 
 def model_ex_predict(model, X, y, title, fn):
     plt.plot(X, model.predict(X), linewidth=3)
 
     plt.title(title, fontsize=24)
-    plt.xlabel('Head Size (in^3)', fontsize=18)
-    plt.ylabel('Brain Weight (lb)', fontsize=18)
-    plt.xlim(180,280)
-    plt.ylim(2.4,3.8)
+
 
     plt.plot([240,240],[2.4,3.03], color='r')
     plt.plot([180,240],[3.03,3.03], color='r')
-    plt.tick_params(labelsize=14)
-    plt.savefig(fn)
-
-    plt.show()
+    design_plot(fn)
 
 def main():
     data = load_data('./data/both_brain_body.txt')    
